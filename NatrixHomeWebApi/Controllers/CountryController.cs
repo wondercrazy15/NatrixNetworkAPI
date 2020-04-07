01fa2dfd118135aeecfe6252d8d03c2823b9c33e
Darshan <dkprajapati18@hotmail.com>
2020-04-07 14:30:29 +0530

    country controller commit

---
+++ NatrixHomeWebApi/Controllers/CountryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NatrixHomeWebApi.Models;
+
+namespace NatrixHomeWebApi.Controllers
+{
+    public class CountryController : Controller
+    {
+        private ApplicationDbContext _dbContext { get; set; }
+
+        public CountryController(ApplicationDbContext dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+
+        [HttpGet]
+        [Route("api/Country/GetCountry")]
+        public IEnumerable<Country> GetCountry()
+        {
+            var objCountry = _dbContext.Country.ToList();
+            return objCountry;   
+        }
+    }
+}
